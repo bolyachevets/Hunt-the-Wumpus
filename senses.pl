6d53dae3fe24d1058de48fc8416830b6de160270
Andrew Bolyachevets <andrew.bolyachevets@utoronto.ca>
2018-11-20 17:56:23 -0800

    fixed sense: can feel bats and/or pit

--- senses.pl
+++ senses.pl
@@ -3,21 +3,22 @@
 next_to_pit :-
     current_room(Current),
     connected(Current, X),
-    bottomless_pit(X),
-    write("I feel a breeze..."), nl.
+    bottomless_pit(X).
 
 next_to_bat_cave :-
     current_room(Current),
     connected(Current, X),
-    bat_cave(X),
-    write("I hear'em flapping..."), nl.
+    bat_cave(X).
 
 senses_check :-
-    next_to_pit,
-    next_to_bat_cave,
-    print_room;
-    next_to_pit,
-    print_room;
-    next_to_bat_cave,
-    print_room.
+   next_to_pit,
+   next_to_bat_cave,
+   write("Double trouble!!! Both bats and pit nearby..."), nl,
+   print_room;
+   next_to_pit,
+   write("I feel a breeze..."), nl,
+   print_room;
+   next_to_bat_cave,
+   write("I hear'em flapping..."), nl,
+   print_room.
 
