36cd681c4cafda52f15117f1c877a2ade0fa38ce
TruemanL <truemanlam@gmail.com>
2018-11-21 18:13:26 -0800

    added ability to shoot arrows across up to 5 connected rooms now

--- actions.pl
+++ actions.pl
@@ -4,11 +4,7 @@ change_room(NewRoom) :-
     assertz(current_room(NewRoom)),
     events; senses_check.
 
-shoot_arrow(Aim) :-
-    % update target
-    target(Old),
-    retract(target(Old)),
-    assertz(target(Aim)),
+shoot_arrow(Current, Targets) :-
     % update arrow count
     quiver(Arrows),
     decr(Arrows, DecArrows),
@@ -16,12 +12,39 @@ shoot_arrow(Aim) :-
     assertz(quiver(DecArrows)),
     print(DecArrows),
     write(" arrows left..."), nl,
-    events.
-
-arrow_pass_through(Aim) :-
+    retractall(energy(_)),
+    assertz(energy(5)),
+    arrow_pass_through(Current, Targets).
+    
+arrow_pass_through(PreviousTarget, Targets) :-
     % update target
+    energy(ArrowEnergy),
+    decr(ArrowEnergy, EnergyLeft),
+    retract(energy(ArrowEnergy)),
+    assertz(energy(EnergyLeft)),
+    resolve_arrow(PreviousTarget, Targets).
+    
+resolve_arrow(PreviousTarget, [Aim|NextTargets]) :- 
+    energy(ArrowEnergy),
+    ArrowEnergy > 0,
+    connected(PreviousTarget, Aim),
+    write("arrow is flying through room "),
+    print(Aim), nl,
+    (check_room_for_hit(Aim);
+    (list_empty(NextTargets, false),
+    ArrowEnergy > 0,
+    arrow_pass_through(Aim, NextTargets))).
+    
+resolve_arrow(_, _) :-
+    energy(ArrowEnergy),
+    ArrowEnergy is 0, 
+    write("arrow ran out of kinetic energy and crashed into the ground"), nl.
+    
+check_room_for_hit(Aim) :-
     target(Old),
     retract(target(Old)),
     assertz(target(Aim)),
     events.
-
+    
+list_empty([], true).
+list_empty([_|_], false).
